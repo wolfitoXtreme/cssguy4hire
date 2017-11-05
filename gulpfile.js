005a26e007df408901257989fa51677627f36ccf
Cesar Candela <cesar@cssguy4hire.com>
2017-11-05 17:52:37 +0200

    #20 - Added task dependencies needed when running single a task.

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,7 @@ var gulp = require('gulp'),
 gulp.task('browserify', config.functions.getTask(gulp, 'browserify', plugins, config));
 
 // compass
-gulp.task('compass', config.functions.getTask(gulp, 'compass', plugins, config));
+gulp.task('compass', ['compass-vars'], config.functions.getTask(gulp, 'compass', plugins, config));
     gulp.task('compass-vars', config.functions.getTask(gulp, 'compass-vars', plugins, config));
         gulp.task('compass-vars-breakpoints', config.functions.getTask(gulp, 'compass-vars-breakpoints', plugins, config));
         gulp.task('compass-vars-icons', config.functions.getTask(gulp, 'compass-vars-icons', plugins, config));
@@ -24,14 +24,14 @@ gulp.task('compass', config.functions.getTask(gulp, 'compass', plugins, config))
 // svg-icons
 gulp.task('svg-icons', config.functions.getTask(gulp, 'svg-icons', plugins, config));
     gulp.task('svg-icons-store', config.functions.getTask(gulp, 'svg-icons-store', plugins, config));
-    gulp.task('svg-icons-data', config.functions.getTask(gulp, 'svg-icons-data', plugins, config));
-    gulp.task('svg-icons-reference', config.functions.getTask(gulp, 'svg-icons-reference', plugins, config));
+    gulp.task('svg-icons-data', ['svg-icons-store'], config.functions.getTask(gulp, 'svg-icons-data', plugins, config));
+    gulp.task('svg-icons-reference', ['svg-icons-data'], config.functions.getTask(gulp, 'svg-icons-reference', plugins, config));
 
 // images
 gulp.task('images', config.functions.getTask(gulp, 'images', plugins, config));
     gulp.task('images-min', config.functions.getTask(gulp, 'images-min', plugins, config));
-    gulp.task('images-data', config.functions.getTask(gulp, 'images-data', plugins, config));
-    gulp.task('images-reference', config.functions.getTask(gulp, 'images-reference', plugins, config));
+    gulp.task('images-data', ['images-min'], config.functions.getTask(gulp, 'images-data', plugins, config));
+    gulp.task('images-reference', ['images-data'], config.functions.getTask(gulp, 'images-reference', plugins, config));
 
 // html
 gulp.task('html', config.functions.getTask(gulp, 'html', plugins, config));
