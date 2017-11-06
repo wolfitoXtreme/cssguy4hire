e2b44eb0d4b10c24e4796cf1778c5ee42a911c47
Cesar Candela <cesar@cssguy4hire.com>
2017-11-06 23:28:38 +0200

    #22 - Added method to initialize app only after loading the font files.

--- app/js/dev/main.js
+++ app/js/dev/main.js
@@ -21,72 +21,105 @@ var domAdjust = require('./domAdjust'),
 // app
 $(function() {
 
-    // initializing loaded modules
-    domAdjust.init();
-    emailProtector.init();
-    breakpoints.init();
-    svgIcons.init();
-    panelNav.init();
-    slider.init();
+    var app = {
+        init: function() {
+            domAdjust.init();
+            emailProtector.init();
+            breakpoints.init();
+            svgIcons.init();
+            panelNav.init();
+            slider.init();
 
-    // test enquire
-    enquire.register('screen and (min-width:' + breakpoints.get('xsmall') + ') and (max-width:' + breakpoints.get('small') + ')', {
-        match : function() {
-            console.log('Breaking point ' + 'xsmall' + ' reached!');
-        },
-        unmatch : function() {
-            console.log('Breaking point ' + 'xsmall' + ' exit!');
-        }
-    })
-    .register('screen and (min-width:' + breakpoints.get('small') + ') and (max-width:' + breakpoints.get('medium') + ')', {
-        match : function() {
-            console.log('Breaking point ' + 'small' + ' reached!');
-        },
-        unmatch : function() {
-            console.log('Breaking point ' + 'small' + ' exit!');
-        }
-    })
-    .register('screen and (min-width:' + breakpoints.get('medium') + ') and (max-width:' + breakpoints.get('large') + ')', {
-        match : function() {
-            console.log('Breaking point ' + 'medium' + ' reached!');
-        },
-        unmatch : function() {
-            console.log('Breaking point ' + 'medium' + ' exit!');
-        }
-    })
-    .register('screen and (min-width:' + breakpoints.get('large') + ')', {
-        match : function() {
-            console.log('Breaking point ' + 'large' + ' reached!');
-        },
-        unmatch : function() {
-            console.log('Breaking point ' + 'large' + ' exit!');
-        }
-    });
+            // test enquire
+            enquire.register('screen and (min-width:' + breakpoints.get('xsmall') + ') and (max-width:' + breakpoints.get('small') + ')', {
+                match : function() {
+                    console.log('Breaking point ' + 'xsmall' + ' reached!');
+                },
+                unmatch : function() {
+                    console.log('Breaking point ' + 'xsmall' + ' exit!');
+                }
+            })
+            .register('screen and (min-width:' + breakpoints.get('small') + ') and (max-width:' + breakpoints.get('medium') + ')', {
+                match : function() {
+                    console.log('Breaking point ' + 'small' + ' reached!');
+                },
+                unmatch : function() {
+                    console.log('Breaking point ' + 'small' + ' exit!');
+                }
+            })
+            .register('screen and (min-width:' + breakpoints.get('medium') + ') and (max-width:' + breakpoints.get('large') + ')', {
+                match : function() {
+                    console.log('Breaking point ' + 'medium' + ' reached!');
+                },
+                unmatch : function() {
+                    console.log('Breaking point ' + 'medium' + ' exit!');
+                }
+            })
+            .register('screen and (min-width:' + breakpoints.get('large') + ')', {
+                match : function() {
+                    console.log('Breaking point ' + 'large' + ' reached!');
+                },
+                unmatch : function() {
+                    console.log('Breaking point ' + 'large' + ' exit!');
+                }
+            });
 
-    // test onScreenTest
-    $(window).on({
-        'resize.test': function(event) {
-            $('.main').height($(window).height());
+            // test onScreenTest
+            $(window).on({
+                'resize.test': function(event) {
+                    $('.main').height($(window).height());
 
-            onScreenTest.test(
-               'TEST WINDOW RESIZE', [
-                   'window width = ' + $(window).width(),
-                   'window height = ' + $(window).height(),
-                   'window innerWidth = ' + window.innerWidth,
-                   'window innerHeight = ' + window.innerHeight,
-                   'window outerWidth = ' + window.outerWidth,
-                   'window outerHeight = ' + window.outerHeight,
-                   'screen availHeight = ' + screen.availHeight,
-                   'screen availWidth = ' + screen.availWidth,
-                   'document width = ' + $(document).width(),
-                   'document height = ' + $(document).height(),
-                   'body width = ' + $('body').width(),
-                   'body height = ' + $('body').height(),
-                   'body clientWidth = ' + $('body').prop('clientWidth'),
-                   'body clientHeight = ' + $('body').prop('clientHeight'),
-                   'body STYLE = ' + $('body').attr('style')
-               ], false
-            );
+                    onScreenTest.test(
+                       'TEST WINDOW RESIZE', [
+                           'window width = ' + $(window).width(),
+                           'window height = ' + $(window).height(),
+                           'window innerWidth = ' + window.innerWidth,
+                           'window innerHeight = ' + window.innerHeight,
+                           'window outerWidth = ' + window.outerWidth,
+                           'window outerHeight = ' + window.outerHeight,
+                           'screen availHeight = ' + screen.availHeight,
+                           'screen availWidth = ' + screen.availWidth,
+                           'document width = ' + $(document).width(),
+                           'document height = ' + $(document).height(),
+                           'body width = ' + $('body').width(),
+                           'body height = ' + $('body').height(),
+                           'body clientWidth = ' + $('body').prop('clientWidth'),
+                           'body clientHeight = ' + $('body').prop('clientHeight'),
+                           'body STYLE = ' + $('body').attr('style')
+                       ], false
+                    );
+                }
+            });
         }
-    });
+    }
+
+    // Initialize app after fonts loading
+    fontDelay(app.init());
+
+    // font delay
+    function fontDelay(execFunction) {
+
+        var checkFontsCount = 0,
+            checkFonts = setInterval(function() {
+
+                checkFontsCount += 1;
+                
+                if(typeof window.fontsReady !== 'undefined') {
+
+                    onScreenTest.test(
+                       'TEST FONTS LOADING', [
+                           'checkFontsCount = ' + checkFontsCount,
+                           'fontsReady = ' + window.fontsReady
+                       ], false
+                    );
+                
+                    if(window.fontsReady !== false) {
+                        execFunction;
+                        clearInterval(checkFonts);
+                    }
+                }
+                
+            }, 100);
+    }
+
 });
