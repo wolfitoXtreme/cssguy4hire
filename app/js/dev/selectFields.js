a5a5e50e6d023105eedca9eb15089165117755ec
Cesar Candela <cesar@cssguy4hire.com>
2017-12-06 21:43:16 +0200

    #32 - Refactored SelectBox replacement for mobile devices. Increased default fields witdth.

--- app/js/dev/selectFields.js
+++ app/js/dev/selectFields.js
@@ -4,19 +4,6 @@ var selectbox = require('selectbox'),
     debounce = require('lodash/debounce'),
     isMobile = require('ismobilejs');
 
-
-function mobileSelect($select, $selectBox) {
-    var $selectBoxLabel = $selectBox.find('.selectBox-label');
-
-    $select.on({
-        'change': function(event) {
-            var currentValue = $select.find(':selected').text();
-            console.log('change');
-            $selectBoxLabel.text(currentValue);
-        }
-    });
-}
-
 // 
 // initialize selectFields
 // 
@@ -42,31 +29,50 @@ var selectFields = {
                     $select.before($selectWrapper);
                     $select.appendTo($selectWrapper);
 
-                });
-            }
-
-            // initialize selectBox plugin
-            this.selects.selectBox(this.selectBoxOptions);
-
-            // if mobile, native select takes over select behavior
-            if(this.isMobile) {
-                this.selects.each(function(i){
-                    var $select = $(this),
-                        $selectBox = $select.next('.selectBox-dropdown');
-
-                    mobileSelect($select, $selectBox);
-
-                    // remove click events added by selectBox
-                    $select.off('click');
-                    $selectBox.off('mousedown');
+                    selectFields.mobile($select);
                 });
             }
             else {
+                // initialize selectBox plugin
+                this.selects.selectBox(this.selectBoxOptions);
                 // handle resizing for non mobile devices
                 this.resize();
             }
         }
     },
+
+    // mobile version
+    mobile: function(select) {
+        var $select = select,
+            $label = $('<span class="selectBox-label" />'),
+            $arrow = $('<span class="selectBox-arrow" />'),
+            $dropDown = $('<div class="selectBox-dropdown" />'),
+            $arrowIcon = $('<svg class="selectBox-arrow__icon" width="0" height="0"><use xlink:href="#ui-select-arrow" /></svg>'),
+            selectClass = $select.attr('class');
+        
+        // include arrow icon
+        $arrowIcon.appendTo($arrow);
+
+        // attach classes and components to dropDown 
+        $dropDown.addClass(selectClass).append($label, $arrow);
+
+        // insert dropDown after select field
+        $select.after($dropDown);
+
+        // add label value
+        $label.text($select.find(':selected').text());
+
+        // handle on change events
+        $select.on({
+            'change': function(event) {
+                var currentValue = $select.find(':selected').text();
+                $label.text(currentValue);
+            }
+        });
+
+    },
+
+    // resize events to update options width
     resize: function() {
         $(window).on({
             'resize.select.debounce': debounce(
