6ee33ac890c61032fba456408f0c1383a3bf0208
Cesar Candela <cesar@cssguy4hire.com>
2017-12-26 17:51:09 +0200

    #49 - Added menus JS hooks. Added Menu heading classes. Removed unnecessary class names. Added Utility menu icons, mark-up and class names.

--- app/js/dev/domAdjust.js
+++ app/js/dev/domAdjust.js
@@ -10,8 +10,16 @@ var domAdjust = {
         this.cover =        this.panels.eq(0).clone().empty();
         this.signature =    $('.js-footer').find('.footer-signature').clone();
         this.hidden =       $('.js-hidden');
+        this.utilMenu =     $('.js-nav-util-menu');
 
         console.log('Header ID -->' + domAdjust.header.attr('id'))
+
+        // relocate utilities menu
+        domAdjust.utilMenu.prependTo(domAdjust.header);
+        domAdjust.utilMenu.clone()
+            .removeClass('js-nav-util-menu')
+            .addClass('nav-util-menu--internal')
+            .prependTo(domAdjust.panels);
 
         // apply all original 'header' characteristics to 'cover'
         domAdjust.cover
@@ -27,7 +35,7 @@ var domAdjust = {
         // remove original 'header'
         domAdjust.header.remove();
 
-        // add signature logo to all panels
+        // TODO -- add signature logo to all panels
         domAdjust.signature.attr('class', 'panel__signature');
 
         // relocate 'contact' panel
@@ -35,6 +43,7 @@ var domAdjust = {
 
         // hide all non required markup
         domAdjust.hidden.hide();
+    
     }
 }
 
