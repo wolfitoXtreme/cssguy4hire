2ab06e60273070510f67728f4a395f3c9f6eab82
Cesar Candela <cesar@cssguy4hire.com>
2018-01-20 19:17:29 +0200

    #57 - Removed comments on Slider file.

--- app/js/dev/slider.js
+++ app/js/dev/slider.js
@@ -2,38 +2,7 @@
 
 var throttle = require('lodash/throttle'),
     debounce = require('lodash/debounce'),
-    // slick = require('slick'),
-    // panelNav = require('./panelNav');
     Swiper = require('swiper');
-
-// Default slider options
-// If additional breakpoints configuration is needed, it will be passed as a data attribute
-// e.g. data-slider-settings='{"breakpoints": [[320, {"dots": false}], [576, {"dots": true}]]}'
-// var options = {
-//     slidesToShow: 1,
-//     arrows: true,
-//     dots: false,
-//     dotsClass: '',
-//     infinite: true,
-//     speed: 500,
-//     // adaptiveHeight: true,
-//     centerMode: false,
-//     centerPadding: 0,
-//     mobileFirst: true,
-//     responsive: [],
-//     prevArrow:  '<a href="#" class="slider-arrows__arrow">' +
-//                    '<svg class="slider-arrows__icon" width="0" height="0">' +
-//                         '<use xlink:href="#icon-slider-arrow" />' +
-//                     '</svg>' +
-//                     'previous' + 
-//                 '</a>',
-//     nextArrow: '<a href="#" class="slider-arrows__arrow slider-arrows__arrow--next">' +
-//                    '<svg class="slider-arrows__icon" width="0" height="0">' +
-//                         '<use xlink:href="#icon-slider-arrow" />' +
-//                     '</svg>' + 
-//                     'next' + 
-//                 '</a>'
-// }
 
 // 
 // Sliders
@@ -108,75 +77,9 @@ var slider = {
             slider.swipers['slider_' + sliderIndex] = new Swiper('.' + sliderClassName, sliderOptions);
         });
 
-
-
         console.log('sliders.length = ' + this.sliders.length);
         console.log('sliders swipers.length = ' + slider.swipers.length);
         console.log('slider.swipers.slider_1 = ' + slider.swipers.slider_1);
-
-
-        // var testSlider = new Swiper('slider-container-1', slider.options);
-
-        //  this.panelSwiper = new Swiper('.swiper-container', panelNav.options);
-
-
-        // this.sliders = $('.js-slider');
-
-        // this.sliders.each(function(i) {
-            
-        //     var $slider = $(this),
-        //         settings = $slider.data('slider-settings'),
-        //         responsive = [],
-        //         breakpoints = typeof settings !== 'undefined' ? settings.breakpoints : null;
-
-        //     $slider.options = options;
-
-        //     // if there are any breakpoints defined
-        //     if(breakpoints) {
-        //         console.log('breakpoints found!!');
-        //         for (var breakpoint in breakpoints) {
-        //             var config = {};
-        //             config.breakpoint = breakpoints[breakpoint][0];
-        //             config.settings = breakpoints[breakpoint][1];
-
-        //             responsive.push(config);
-        //         }
-        //     }
-
-        //     // set responsive options settings
-        //     $slider.options.responsive = responsive;
-            
-        //     // $slider.on('init', function(slick) {
-            
-        //     //     $slider.data('slides', $slider.find('.slick-slide'));
-
-        //     //     console.log('slides = > ' + $slider.data('slides').length);
-        //     // }).slick($slider.options);
-
-        //     // slider methods
-        //     $slider.on({
-        //         'init': function(slick) {
-
-        //             var $items = $(this).find('li'),
-        //                 $arrowsContainer = $('<div class="slider-arrows" />'),
-        //                 $focusables = $(':focusable', this).add($items);
-
-        //             // wrap slider arrows inside a container
-        //             $(this).children('.slider-arrows__arrow').appendTo($arrowsContainer);
-        //             $arrowsContainer.appendTo($(this))
-
-        //             // set tab navigation (calling panelNav method)
-        //             panelNav.tabNavigation($focusables);
-        //         }
-        //     });
-
-        //     // initialize slider
-        //     $slider.slick($slider.options);
-        // });
-
-        // // initialize methods
-        // slider.resize(slider.sliders);
-        // slider.matchSize(slider.sliders);
     },
 
     // update panel navigation on resize
