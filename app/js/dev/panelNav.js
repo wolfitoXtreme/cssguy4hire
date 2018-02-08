b56a0fd8fa7d93ec54aaa4d1ec722cbd60a24523
Cesar Candela <cesar@cssguy4hire.com>
2018-02-08 17:20:51 +0200

    #74 - Added arrows animations only once per visited Panel. Sorted code.

--- app/js/dev/panelNav.js
+++ app/js/dev/panelNav.js
@@ -12,10 +12,12 @@ var panelNav = {
         this.panels =           $('.panel', this.wrapper);
         this.currentPanel =     0;
         this.panelLinks =       $('.js-nav-main-menu a');
-        this.panelArrows =      $('.js-nav-panel-prev', panelNav.panels)
-                                .add('.js-nav-panel-next', panelNav.panels);
+        // this.panelArrows =      $('.js-nav-panel-prev', this.panels)
+        //                         .add('.js-nav-panel-next', this.panels);
+        this.panelArrows =      $([]); // empty collection
+        this.allVisited =       0;
         this.focusables =       $(':focusable', this.panels);
-        this.mobileNav; // Store Mobile Nav here
+        this.mobileNav;         // Store Mobile Nav here
         this.options = {
             wrapperClass: 'js-panels',
             slideClass: 'panel',
@@ -37,24 +39,82 @@ var panelNav = {
             },
             on: {
                 init: function() {
-                } ,
+                    var $currentPanel = panelNav.panels.eq(0); // on init always 0
+                    
+                    // show panel navigation arrows animation
+                    // panelNav.hightlightArrows($currentPanel);
+
+                    console.log('panel event PANEL INIT');
+
+                    if(
+                        !$currentPanel.data('visited') ||       // is not visited
+                        !$currentPanel.data('arrowDone')        // arrow animation is not done
+                    ) {
+                        
+                        // panelNav.hightlightArrows($currentPanel, $currentPanel.data('nextArrow'), $previousPanel.data('nextArrow'));
+                        panelNav.hightlightArrows(
+                            $currentPanel,
+                            $currentPanel.data('nextArrow')
+                        );
+                    }
+                },
+
+                slideChange: function() {
+
+                    console.log('panel event PANEL CHANGE');
+                },
+
+                sliderMove: function() {
+                    console.log('panel event PANEL MOVE.');
+                },
+
+                transitionStart: function() {
+                    console.log('panel event PANEL TRANSITION START');
+                },
+
+                transitionEnd: function() {
+                    console.log('panel event PANEL TRANSITION END');
+                },
+
+                slideChangeTransitionStart: function() {
+                    console.log('panel event PANEL CHANGE TRANSITION START');
+                },
 
                 slideChangeTransitionEnd: function() {
                     var currentPanel = panelNav.panelSwiper.activeIndex,
-                        $panels = panelNav.panels;
-
-                    console.log('swiper end change, current is = ' + currentPanel);
+                        previousPanel = panelNav.panelSwiper.previousIndex,
+                        $panels = panelNav.panels,
+                        $currentPanel = $panels.eq(currentPanel),
+                        $previousPanel = $panels.eq(previousPanel);
 
                     // set focus on focus dummy
                     // therefore allowing tab navigation from current panel
-                    $panels.eq(currentPanel).children().filter(
+                    $currentPanel.children().filter(
                         function(index) {
                             return $(this).is('.js-focus-dummy');
                         }
                     ).focus();
 
-                    // set current panel
+                    // set current panel (Mobile.js use)
                     panelNav.currentPanel = currentPanel;
+
+                    // show panel navigation arrows animation only if not previously visited
+                    console.log('nextArrow =' + $currentPanel.data('arrowDone'));
+                    if(
+                        !$currentPanel.data('visited') ||       // is not visited
+                        !$currentPanel.data('arrowDone')        // arrow animation is not done
+                    ) {
+                        
+                        // panelNav.hightlightArrows($currentPanel, $currentPanel.data('nextArrow'), $previousPanel.data('nextArrow'));
+                        panelNav.hightlightArrows(
+                            $currentPanel,
+                            $currentPanel.data('nextArrow'), 
+                            $previousPanel,
+                            $previousPanel.data('nextArrow')
+                        );
+                    }
+
+                    console.log('panel event PANEL CHANGE TRANSITION END = ' + panelNav.panelArrows.length);
                 },
 
                 progress: function(progress){
@@ -80,11 +140,30 @@ var panelNav = {
                 }
             }
         };
-        this.panelSwiper = new Swiper('.js-panels-container', panelNav.options);
 
         // add dummy navigation focus 
         panelNav.panels.prepend($('<span class="js-focus-dummy hidden" tabindex="0" />'));
         panelNav.focusables = panelNav.focusables.add('.js-focus-dummy');
+
+        // store arrows
+        panelNav.panels.each(function(i){
+            var $prevArrow = $('.js-nav-panel-prev', this),
+                $nextArrow = $('.js-nav-panel-next', this);
+            console.log('$nextArrow = ' + $nextArrow.length);
+
+            if($prevArrow.length) panelNav.panelArrows.push($prevArrow);
+            if($nextArrow.length) {
+                panelNav.panelArrows.push($nextArrow);
+                $(this).data({
+                    'visited':          false,
+                    'arrowDone':        false,
+                    'nextArrow':    $nextArrow
+                }); // store nextArrow into this
+            };
+        });
+
+        // initialize panelSwiper
+        panelNav.panelSwiper = new Swiper('.js-panels-container', panelNav.options);
 
         // initialize methods
         panelNav.linkNavigation(this.panelLinks);
@@ -92,7 +171,7 @@ var panelNav = {
         panelNav.arrowNavigation();
 
         // set mobile panel links background colors
-        this.panelLinks.each(function(i){
+        panelNav.panelLinks.each(function(i){
             var panelIndex =  $(this).parent().index();
 
             console.log('panelIndex = ' + panelIndex);
@@ -122,7 +201,8 @@ var panelNav = {
 
     // link navigation
     linkNavigation: function(links) {
-        var $links = links,
+        var $panels = this.panels,
+            $links = links,
             mobileNav = this.mobileNav;
             
         $links.on({
@@ -161,7 +241,7 @@ var panelNav = {
                     var $target = $(event.target),
                         $parentPanel = $target.closest('.panel'),
                         parentIndex = $parentPanel.index(),
-                        currentPanel = panelNav.currentPanel;
+                        currentPanel = panelNav.panelSwiper.activeIndex;
 
                     // if focused element not inside current panel 
                     if(parentIndex !== currentPanel) {
@@ -206,23 +286,104 @@ var panelNav = {
         // });
     },
 
+    // hightlight arrows
+    // hightlightArrows: function($currentPanel, $currentPanelArrow, $previousPanelArrow) {
+    hightlightArrows: function($currentPanel, $currentPanelArrow, $previousPanel, $previousPanelArrow) {
+        var $panels = this.panels;
+
+        // set panel as visited
+        var setVisited = function($panel, $arrow) {
+            $panel.data({
+                'visited': true,
+                'arrowDone': true
+            });
+
+            if(typeof $arrow !== 'undefined') {
+                $arrow.removeClass('nav-panel__link--on-screen nav-panel__link--animated');
+            }
+        }
+
+        // set current panel as visited
+        $currentPanel.data({
+            'visited': true
+        });
+        
+        // set all previous panels as visited ('mobile Panel not linear navigation')
+        if(panelNav.allVisited < $panels.length) {
+            panelNav.allVisited += 1;
+ 
+            $panels.each(function(i){
+                if(i < $currentPanel.index()) {
+                    setVisited($(this), $(this).data('nextArrow'));
+                }
+                else {
+                    return;
+                }
+                console.log('panel looping!!');
+            });
+        }
+
+        // set previous panel as visited, if any
+        if(typeof $previousPanel !== 'undefined'){
+            setVisited($previousPanel, $previousPanelArrow);
+        }
+
+        if(typeof $currentPanelArrow !== 'undefined') {
+
+            $currentPanelArrow.addClass('nav-panel__link--on-screen nav-panel__link--animated').one({
+                'animationend webkitAnimationEnd' : function(event) {
+                    event.stopPropagation();
+
+                    // Avoid running twice the code (':pseudo element ins triggering one more')
+                    $(this).off('animationend webkitAnimationEnd');
+                    
+                    $(this).removeClass('nav-panel__link--animated').one({
+                        'transitionend webkitTransitionEnd' : function(event) {
+                            event.stopPropagation();
+                            event.stopImmediatePropagation();
+                            
+                            // set panel as visited
+                            setVisited($currentPanel, $currentPanelArrow);
+
+                            console.log('nav-panel__link transition END!!! ' + $(event.target).closest('.panel').attr('class'));
+                        
+                            // Avoid running twice the code ('transition is also set for tab navigation use')
+                            $(this).off('transitionend webkitTransitionEnd');
+                        }
+                    });
+
+                    console.log('nav-panel__link animation END!!!');
+                }
+            });
+        }
+        else { // sets visited for Contact panel ('no next arrow')
+            // set panel as visited
+            setVisited($currentPanel, $currentPanelArrow);
+        }
+
+        console.log(
+            '-----\n' +
+            // 'panelNav.allVisited = ' + panelNav.allVisited + '\n' +
+            // '$panels.length = ' + $panels.length + '\n' +
+            // '$currentPanel =' + $currentPanel.attr('class') + '\n' +
+            '$currentPanel index =' + $currentPanel.index() + '\n' +
+            '$currentPanel visited =' + $currentPanel.data('visited') + '\n' +
+            '$currentPanel arrowDone =' + $currentPanel.data('arrowDone') + '\n' +
+            // '$currentPanel arrowAnimating =' + $currentPanel.data('arrowAnimating') + '\n' +
+            '-----\n' +
+            // '$previousPanel index =' + if($previousPanel.index() + '\n' +
+            // '$previousPanel visited =' + $previousPanel.data('visited') + '\n' +
+            // '$previousPanel arrowDone =' + $previousPanel.data('arrowDone') + '\n' +
+            // '$previousPanel arrowAnimating =' + $previousPanel.data('arrowAnimating') + '\n' +
+            // '$currentArrow = ' + $currentPanelArrow + '\n' +
+            // '$previousPanelArrow = ' + $previousPanelArrow
+            '-----\n\n'
+        );
+
+    },
+
     // control panels with mousewheel
     mousewheel: function(panelWrapper) {
-
-        // panelWrapper.on({
-        //     'mousewheel': function(event) {
-        //         console.log('event.deltaY = ' + event.deltaY);
-                
-        //         if(event.deltaY < 0) {
-        //             panelNav.gotoPanel(parentIndex, 'tabNavigation');
-        //         }
-        //         else if(event.deltaY > 0) {
-        //              panelNav.nextPanel();
-        //         }
-
-        //         // console.log('slickCurrentSlide =' + $(panelNav.wrapper).slick('slickCurrentSlide'));
-        //     }
-        // });
     },
 
     animatePanels: function(
@@ -263,7 +424,6 @@ var panelNav = {
             'translate3d(0px, ' + getOffset(panelNav.options.offsetRatio, false) + 'px, 0px)' +
             'scale(' + localProgress + ')'
         ));
-
 
         if($panel.index() === 0) {
             onScreenTest.test(
