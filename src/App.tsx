688bb35985bbe8fd7b4be0196681f0e1a9688dfe
Cesar <cesar@cssguy4hire.com>
2020-12-06 23:21:27 +0200

    #r04 - Add base styles and base typography.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
+import './scss/App.scss';
+
 function App() {
-  return <div>Hello cruel world!</div>;
+  return (
+    <>
+      <h1>Hello cruel world!</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt dolorem
+        cumque tempore blanditiis consectetur voluptatibus, odio adipisci,
+        ducimus tempora, sapiente necessitatibus? Similique at ut expedita eos,
+        consequuntur soluta sit ab!
+      </p>
+    </>
+  );
 }
 
 export default App;
