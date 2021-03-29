cb30f76fa3b640018a3bf330752931d5d18a88cf
Cesar <cesar@cssguy4hire.com>
2021-03-29 17:47:11 +0300

    #r40 - Customizes Menu Swiper-Scrollbar

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import SwiperCore, { Scrollbar, Mousewheel } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/swiper.scss';
-import 'swiper/components/scrollbar/scrollbar.scss';
 
 import { devices } from '@app/types/types';
 
